a03f98b4dd1e271f5622ddd1a2757230f9c3046f
Eman Izhar <166126055+Eizhar2005@users.noreply.github.com>
2026-04-16 08:38:36 +0500

    Initial commit

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div id="parent">
+      <h1 id="child">App</h1>
+      <h1 id="child2" >Hwll</h1>
+      <h1>dfedf</h1>
+    </div>
+    
+  )
+}
+
+export default App
